ca6f875a9446ff4937af252ed06970d8095668be
Kang Peng <kangtingpeng@nypl.org>
2016-05-06 12:37:55 -0400

    Updated routing so /search/apachesolr_search with or without / will go to main page.

--- src/server/ApiRoutes/ApiRoutes.js
+++ src/server/ApiRoutes/ApiRoutes.js
@@ -112,7 +112,7 @@ router
   .get(requestSearchResult);
 
 router
-  .route(/^((?!\/search\/apachesolr_search[\/]).)*$/)
+  .route(/^((?!\/search\/apachesolr_search\/[^]).)*$/)
   .get(mainApp);
 
 export default router;
