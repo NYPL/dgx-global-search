c54fe35cc1c10382438b6950ea3553e4d8f81568
Kang Peng <kangtingpeng@nypl.org>
2016-05-18 16:34:33 -0400

    Set up Search SVG icon.

--- src/app/components/SearchButton/SearchButton.jsx
+++ src/app/components/SearchButton/SearchButton.jsx
@@ -6,7 +6,15 @@ const SearchButton = ({
 }) => (
   <div className={`${className}Wrapper`}>
     <div className={className} onClick={onClick}>
-      SEARCH
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#FFF" viewBox="0 0 32 32">
+        <title>search.icon.v1</title>
+        <path
+          d={'M25.26014,23.627l-3.76-3.75948a8.53632,8.53632,0,1,0-2.07145,1.85393l3.86877,' +
+            '3.86822A1.38781,1.38781,0,0,0,25.26014,23.627ZM9.109,14.52943a5.75012,5.75012,0,1,1,' +
+            '5.75012,5.74948A5.75662,5.75662,0,0,1,9.109,14.52943Z'}
+        />
+      </svg>
+      <p>SEARCH</p>
     </div>
   </div>
 );
