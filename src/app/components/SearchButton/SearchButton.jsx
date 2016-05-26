36813e2cf0769628560477bddcf76e8c6bbfaded
Kang Peng <kangtingpeng@nypl.org>
2016-05-26 17:50:14 -0400

    Updated Search Icon.

--- src/app/components/SearchButton/SearchButton.jsx
+++ src/app/components/SearchButton/SearchButton.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { SearchIcon } from 'dgx-svg-icons';
+
 const SearchButton = ({
   id,
   className,
@@ -8,14 +10,15 @@ const SearchButton = ({
 }) => (
   <div id={`${id}-wrapper`} className={`${className}-wrapper`}>
     <button id={id} className={className} onClick={onClick}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#FFF" viewBox="0 0 32 32">
-        <title>search.icon.v1</title>
-        <path
-          d={'M25.26014,23.627l-3.76-3.75948a8.53632,8.53632,0,1,0-2.07145,1.85393l3.86877,' +
-            '3.86822A1.38781,1.38781,0,0,0,25.26014,23.627ZM9.109,14.52943a5.75012,5.75012,0,1,1,' +
-            '5.75012,5.74948A5.75662,5.75662,0,0,1,9.109,14.52943Z'}
-        />
-      </svg>
+      <SearchIcon
+        ariaHidden={true}
+        className={`${className}-SearchIcon`}
+        fill="#FFF"
+        height="32"
+        title="search.icon.svg"
+        width="32"
+        viewBox="0 0 32 32"
+      />
       <p>{label}</p>
     </button>
   </div>
