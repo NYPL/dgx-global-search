0afe135d74385c7eed72f315281a6a58488e584d
Kang Peng <kangtingpeng@nypl.org>
2016-05-26 14:40:23 -0400

    Updated apply icon.

--- src/app/components/FilterList/FilterList.jsx
+++ src/app/components/FilterList/FilterList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { ResetIcon, FilterIcon } from 'dgx-svg-icons';
+import { ApplyIcon, ResetIcon, FilterIcon } from 'dgx-svg-icons';
 
 import CloseButton from '../CloseButton/CloseButton.jsx';
 
@@ -35,7 +35,7 @@ class FilterList extends ClickOutComponent {
         <div className={`${this.props.className}-navigation`}>
           <FilterIcon
             aria-hidden="true"
-            className={`${this.props.className}-icon`}
+            className={`${this.props.className}-filterIcon`}
             fill="#FFF"
             height="46"
             width="46"
@@ -45,29 +45,15 @@ class FilterList extends ClickOutComponent {
           <h4>Filter by</h4>
           <div className={`${this.props.className}-buttonWrapper`}>
             <button className="apply">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32" height="32"
+              <ApplyIcon
+                aria-hidden="true"
+                className={`${this.props.className}-ApplyIcon`}
                 fill="#FFF"
+                height="32"
+                width="32"
+                title="apply.icon.svg"
                 viewBox="0 0 32 32"
-              >
-                <title>apply.icon.svg</title>
-                <path
-                  d={'M23.26,13.1819a1.2736,1.2736,0,0,0-1.7332,0L17,17.6253V6.1041a1.0119,' +
-                    '1.0119,0,1,0-2,0V17.6253l-4.5268-4.4434a1.2212,1.2212,0,0,0-1.6916,0,1.17,' +
-                    '1.17,0,0,0-.0208,1.65L15.1786,21.26l0,0.0083a1.1694,1.1694,0,0,0,1.6488,' +
-                    '0l0.0048-.0083L23.26,14.8318A1.17,1.17,0,0,0,23.26,13.1819Z'}
-                />
-                <rect
-                  x="14.8333"
-                  y="16.3602"
-                  width="2.3333"
-                  height="16.6711"
-                  rx="1.1667"
-                  ry="1.1667"
-                  transform="translate(-8.6957 40.6957) rotate(-90)"
-                />
-              </svg>
+              />
             </button>
             <CloseButton
               className="closeButton"
