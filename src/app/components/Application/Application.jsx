a539d665ffdfdd3be2b576c267d864f4f003a7b0
Kang Peng <kangtingpeng@nypl.org>
2016-07-08 12:53:18 -0400

    Removed comments.

--- src/app/components/Application/Application.jsx
+++ src/app/components/Application/Application.jsx
@@ -157,35 +157,6 @@ class App extends React.Component {
         Actions.updateIsKeywordValid(false);
       }
     );
-
-
-    // const currentSearchKeyword = this.state.searchKeyword.trim() || '';
-    // const searchFilter = (facet) ? ` more:${facet}` : '';
-    // const requestParameter = `${currentSearchKeyword}${searchFilter}`;
-
-    // if (!currentSearchKeyword) {
-    //   this.setState({ isKeywordValid: false });
-    // } else {
-    //   axios
-    //   .get(`/api/${requestParameter}?start=0`)
-    //   .then((response) => {
-    //     const { searchResultsItems, resultLength } = response.data;
-
-    //     history.push({
-    //       pathname: `/search/apachesolr_search/${currentSearchKeyword}/${facet}`,
-    //     });
-
-    //     // The functions of Actions.js update the Store with different feature values
-    //     Actions.updateSearchKeyword(currentSearchKeyword);
-    //     Actions.updateSearchData(searchResultsItems);
-    //     Actions.updateSearchDataLength(resultLength);
-    //     Actions.updateSelectedFacet(facet);
-    //     Actions.updateResultsStart(0);
-    //   })
-    //   .catch(error => {
-    //     console.log(`error calling API to search '${requestParameter}': ${error}`);
-    //   });
-    // }
   }
 
   /**
