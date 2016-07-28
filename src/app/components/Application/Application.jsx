1946972c56b57e12b7b5b60e0f0845f02cb3c629
Kang Peng <kangtingpeng@nypl.org>
2016-07-28 13:26:38 -0400

    Updated history mechanism.

--- src/app/components/Application/Application.jsx
+++ src/app/components/Application/Application.jsx
@@ -26,8 +26,8 @@ history.listen(location => {
     action,
     pathname,
   } = location;
-  const searchKeyword = (pathname.split('/')[3]) ? pathname.split('/')[3] : '';
-  const searchFacet = (pathname.split('/')[4]) ? pathname.split('/')[4] : '';
+  const searchKeyword = (pathname.split('/')[2]) ? pathname.split('/')[2] : '';
+  const searchFacet = (pathname.split('/')[3]) ? pathname.split('/')[3] : '';
   const resultsStart = 0;
 
   if (action === 'POP') {
