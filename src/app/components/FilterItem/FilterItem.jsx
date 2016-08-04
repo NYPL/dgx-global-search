262fdd89ff567251ffb61f8cb7f5de94bd18ffe0
Kang Peng <kangtingpeng@nypl.org>
2016-08-04 14:01:01 -0400

    Added onKeyPress's props validation.

--- src/app/components/FilterItem/FilterItem.jsx
+++ src/app/components/FilterItem/FilterItem.jsx
@@ -28,6 +28,7 @@ FilterItem.propTypes = {
   id: React.PropTypes.string,
   className: React.PropTypes.string,
   onClick: React.PropTypes.func,
+  onKeyPress: React.PropTypes.func,
   label: React.PropTypes.string,
 };
 
