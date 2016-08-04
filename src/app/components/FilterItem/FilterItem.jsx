61d82f7d464e4f176b49840ba815e2194799092c
Kang Peng <kangtingpeng@nypl.org>
2016-08-04 13:59:34 -0400

    Updated FilterItem to pure function.

--- src/app/components/FilterItem/FilterItem.jsx
+++ src/app/components/FilterItem/FilterItem.jsx
@@ -3,33 +3,26 @@ import React from 'react';
 // Import libraries
 import { CircleDashIcon } from 'dgx-svg-icons';
 
-class FilterItem extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onKeyPress = this.onKeyPress.bind(this);
-  }
-
-  onKeyPress(event) {
+const FilterItem = ({
+  className,
+  onClick,
+  onKeyPress = (event) => {
     if (event && event.charCode === 13) {
-      this.props.onClick();
+      onClick();
     }
-  }
-
-  render() {
-    return (
-      <li
-        className={this.props.className}
-        onClick={this.props.onClick}
-        onKeyPress={this.onKeyPress}
-        tabIndex="0"
-      >
-        {this.props.label}
-        <CircleDashIcon className={`circleDashIcon ${this.props.className}`} />
-      </li>
-    );
-  }
-}
+  },
+  label,
+}) => (
+  <li
+    className={className}
+    onClick={onClick}
+    onKeyPress={onKeyPress}
+    tabIndex="0"
+  >
+    {label}
+    <CircleDashIcon className={`circleDashIcon ${className}`} />
+  </li>
+);
 
 FilterItem.propTypes = {
   id: React.PropTypes.string,
