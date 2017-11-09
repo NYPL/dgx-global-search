d2998478e16ede8b90716e037036452e820006fd
Kang Peng <kangtingpeng@nypl.org>
2017-11-09 13:10:19 -0500

    Updated the checks for GA config based on the environments.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import alt from 'dgx-alt-center';
-import { gaUtils } from 'dgx-react-ga';
+import { config, gaUtils } from 'dgx-react-ga';
 import a11y from 'react-a11y';
 import FeatureFlags from 'dgx-feature-flags';
 import Iso from 'iso';
@@ -18,9 +18,10 @@ if (loadA11y) {
 
 window.onload = () => {
   if (!window.ga) {
-    const gaOpts = { debug: false, titleCase: false };
+    const isProd = process.env.NODE_ENV === 'production';
+    const gaOpts = { debug: !isProd, titleCase: false };
 
-    gaUtils.initialize('UA-1420324-3', gaOpts);
+    gaUtils.initialize(config.google.code(isProd), gaOpts);
   }
 
   if (!window.dgxFeatureFlags) {
