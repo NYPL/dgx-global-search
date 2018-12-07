7b2e547bebab60b547ae17bec08e066301ad347f
Daniel Appel <dan.s.appel@gmail.com>
2018-12-07 11:22:53 -0500

    Add aria-labelledby to search results

--- src/app/components/Results/Results.jsx
+++ src/app/components/Results/Results.jsx
@@ -281,6 +281,7 @@ class Results extends React.Component {
           selectedFacet={this.props.selectedFacet}
           searchBySelectedFacetFunction={this.props.searchBySelectedFacetFunction}
           saveSelectedTabValue={this.saveSelectedTabValue}
+          results={() => this.refs['results']}
         />
         {(typeof results.length !== 'undefined') && results.length !== 0 &&
           <div>
@@ -296,9 +297,9 @@ class Results extends React.Component {
               viewBox="0 0 84 4"
               width="84"
             />
-            <ol id={this.props.id} className={this.props.className} ref="results">
+          <ol id={this.props.id} className={this.props.className} ref="results" tabIndex='0' aria-labelledby={`link${this.state.tabIdValue}`}>
               {results}
-            </ol>
+          </ol>
             {
               results.length % 10 === 0 &&
               this.renderSeeMoreButton(Math.min(this.props.amount - results.length, 10))
