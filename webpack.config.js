12eec229c331491749be772689b0abd4662cd75e
Daniel Appel <dan.s.appel@gmail.com>
2018-12-10 14:23:50 -0500

    Add encrypted endpoint

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,26 @@ const merge = require('webpack-merge');
 const cleanBuild = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const pkg = require('./package.json');
-
+const kms = require('./lib/kms-helper.js');
+const appConfig = require('./appConfig');
 // References the applications root path
 const ROOT_PATH = path.resolve(__dirname);
 
 // Sets the variable as either development or production
 const ENV = process.env.NODE_ENV || 'development';
+
+// Sets the API_ROOT
+
+let encryptedRoot;
+console.log('apconfig:', appConfig)
+if (ENV === 'development') {
+  encryptedRoot = appConfig.default.searchApi.development;
+} else if (ENV === 'production') {
+  encryptedRoot = appConfig.default.searchApi.production;
+}
+kms.setProfile(process.env.AWS_PROFILE)
+kms.decrypt(encryptedRoot).then(result => {console.log('decrypted:', result); process.env.API_ROOT = result.slice(1, result.length-1)})
+console.log('API_ROOT: ', process.env.API_ROOT)
 
 // Sets appEnv so the the header component will point to the search app on either Dev or Prod
 const appEnv = process.env.APP_ENV ? process.env.APP_ENV : 'production';
