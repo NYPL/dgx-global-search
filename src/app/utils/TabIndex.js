f01465638c23a4d77c795aac96fb7cc18a247619
Daniel Appel <dan.s.appel@gmail.com>
2018-12-20 14:22:20 -0500

    Change tab labels from numbers to names

--- src/app/utils/TabIndex.js
+++ src/app/utils/TabIndex.js
@@ -1,5 +1,7 @@
 import { filterNames } from './FilterNames.js'
 
-const getNumberForFacet = facet => filterNames.map(name => name.value).indexOf(facet) + 1
+// const getNumberForFacet = facet => filterNames.map(name => name.value).indexOf(facet) + 1
+
+const getNumberForFacet = facet => `_${facet}`
 
 export default getNumberForFacet;
