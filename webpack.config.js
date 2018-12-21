78ea71204ec547dba9b687fbbb6118aa05c72ae9
Daniel Appel <dan.s.appel@gmail.com>
2018-12-21 14:05:27 -0500

    Change kms-helper to set profile only if one is given

--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,8 @@ const appEnv = process.env.APP_ENV ? process.env.APP_ENV : 'production';
 // set API_ROOT to the correct encrypted value
 process.env.API_ROOT = ENV === 'development' ? development : production;
 
-if (process.env.AWS_PROFILE) {
-  aws.setProfile(process.env.AWS_PROFILE)
-}
-
-aws.decrypt(process.env.API_ROOT).then(result => {process.env.API_ROOT = result.slice(1, result.length-1)})
+aws.setProfile(process.env.AWS_PROFILE);
+aws.decrypt(process.env.API_ROOT).then(result => {process.env.API_ROOT = result.slice(1, result.length-1)});
 console.log(process.env.API_ROOT)
 
 // Holds the common settings for any environment
