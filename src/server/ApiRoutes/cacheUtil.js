030c7175c41354be8d381a9e3e6b897ea3864a6d
Daniel Appel <dan.s.appel@gmail.com>
2018-12-26 15:11:00 -0500

    Refactor cacheUtil into object

--- src/server/ApiRoutes/cacheUtil.js
+++ src/server/ApiRoutes/cacheUtil.js
@@ -2,26 +2,34 @@ import redis from 'redis';
 
 export default (dataFunction, skipCaching) => {
   if (skipCaching) {
-    return dataFunction;
+    return {
+      getSearchData: dataFunction,
+    };
   }
 
   const client = redis.createClient();
 
+  const cacheUtil = {};
+
   // This wraps the redis client's get method in a promise, so we can avoid
   // nested callbacks
-  const checkForKeyInClient = key => new Promise((resolve) => {
+  cacheUtil.checkForKeyInRedis = key => new Promise((resolve) => {
     client.get(key, (err, cachedValue) => resolve(cachedValue));
   });
 
-  const getDataAndSetKeyInClient = url => dataFunction(url).then((apiResponse) => {
+  cacheUtil.getDataAndSetKeyInClient = url => dataFunction(url).then((apiResponse) => {
     const stringifiedResponse = JSON.stringify(apiResponse);
     client.set(url, stringifiedResponse, 'EX', 3600);
     return stringifiedResponse;
   });
 
-  const useCachedOrGetData = url => checkForKeyInClient(url)
-    .then(redisResponse => (redisResponse === null ? getDataAndSetKeyInClient(url) : redisResponse));
+  cacheUtil.useCachedOrGetData = url => cacheUtil.checkForKeyInRedis(url)
+    .then(redisResponse => (redisResponse === null
+      ? cacheUtil.getDataAndSetKeyInClient(url)
+      : redisResponse));
 
-  return url => useCachedOrGetData(url)
+  cacheUtil.getSearchData = url => cacheUtil.useCachedOrGetData(url)
     .then(stringifiedSearchData => JSON.parse(stringifiedSearchData));
+
+  return cacheUtil;
 };
