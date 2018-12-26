54fe31b3ca36e1bfa0447ba75a9749363a97af41
Daniel Appel <dan.s.appel@gmail.com>
2018-12-26 11:48:51 -0500

    Add comments to TabIndex

--- src/app/utils/TabIndex.js
+++ src/app/utils/TabIndex.js
@@ -2,14 +2,49 @@ import { filterNames } from './FilterNames';
 
 const values = filterNames.map(filter => filter.value);
 
+/**
+ * incrementIndex(arr, index, step)
+ * Adds the step to the index, unless this forces the index out of bounds
+ *
+ * @param {array} arr - An array
+ * @param {int} index - The index of a position in the array
+ * @param {int} step - The amount to increase/decrease the index
+ */
+
 const incrementIndex = (arr, index, step) => {
   const newIndex = index + step;
   return newIndex > -1 && newIndex < arr.length ? newIndex : index;
 };
 
+/**
+ * incrementTab(name, step)
+ * Returns the name of a tab which has distance equal to the given step distance
+ * from the tab with the given name, as long as one exists. Otherwise, returns the original tab name
+ *
+ * @param {string} name - the name of the starting tab
+ * @param {int} step - the distance of the return tab from the starting tab
+ */
+
 const incrementTab = (name, step) => values[incrementIndex(values, values.indexOf(name), step)];
 
+/**
+* getNameForFacet(facet)
+ * Returns the name for a facet. This is the name used for internal logic, e.g.,
+ * comparing the facet name to the selectedFacet
+ *
+ * @param {string} facet - the value property of the facet
+ */
+
 const getNameForFacet = facet => `${facet}`;
+
+/**
+ * displayNameForFacet(facet)
+ * Returns the display name for the facet.
+ * This is the name that will be used in properties shown to the user
+ * For example aria-labelledby uses the display name
+ *
+ * @param {string} facet - the value property of the facet
+ */
 
 const displayNameForFacet = facet => (facet === '' ? 'all' : facet);
 
