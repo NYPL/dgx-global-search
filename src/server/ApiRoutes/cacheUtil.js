96a1fd02875d51902b688d81ebb507f27c8ae29b
Daniel Appel <dan.s.appel@gmail.com>
2018-12-28 14:52:21 -0500

    Fix cachUtil tests to match new expectations

--- src/server/ApiRoutes/cacheUtil.js
+++ src/server/ApiRoutes/cacheUtil.js
@@ -1,38 +1,40 @@
 import redis from 'redis';
 
-const client = redis.createClient();
-
 const getKeyFromParams = params => params.map(x => JSON.stringify(x)).join('');
 
-const checkForKeyInRedis = key => new Promise(resolve => {
-  client.get(key, (err, cachedResponse) => resolve(cachedResponse));
-});
+const checkForKeyInRedis = client => key => new Promise(resolve => client
+  .get(key, (err, cachedResponse) => resolve(cachedResponse)));
 
-const useCachedOrGetData = (dataFunction, params) => {
-  const key = getKeyFromParams(params);
-  return checkForKeyInRedis(key)
-    .then(
-      redisResponse => (redisResponse === null ? getDataAndSetClientKey(dataFunction, params, key) : redisResponse)
-    );
-};
-
-const getDataAndSetClientKey = (dataFunction, params, key) => dataFunction(...params)
-  .then(response => {
-    console.log('here', params);
+const getDataAndSetClientKey = (dataFunction, client) => (params, key) => dataFunction(...params)
+  .then((response) => {
     const stringifiedResponse = JSON.stringify(response);
     // Note that this is asynchronous, but shouldn't matter in this simplest case
     client.set(key, stringifiedResponse, 'EX', 3600);
     return stringifiedResponse;
   });
 
-const addCaching = (dataFunction, useClient) => {
+const useCachedOrGetData = (dataFunction, client) => (params) => {
+  const key = getKeyFromParams(params);
+  return checkForKeyInRedis(client)(key)
+    .then(
+      redisResponse => (redisResponse === null
+        ? getDataAndSetClientKey(dataFunction, client)(params, key)
+        : redisResponse),
+    );
+};
 
+
+const addCaching = (dataFunction, customClient = null, useClient = true) => {
   if (!useClient) {
     return dataFunction;
   }
 
-  return (...params) => useCachedOrGetData(dataFunction, params)
+  const client = customClient || redis.createClient();
+
+  return (...params) => useCachedOrGetData(dataFunction, client)(params)
     .then(stringifiedData => JSON.parse(stringifiedData));
 };
 
-export default { addCaching, getDataAndSetClientKey, useCachedOrGetData, checkForKeyInRedis}
+export default {
+  addCaching, getDataAndSetClientKey, useCachedOrGetData, checkForKeyInRedis, getKeyFromParams,
+};
--- test/cacheUtil.test.js
+++ test/cacheUtil.test.js
@@ -1,26 +1,30 @@
-import sinon from 'sinon';
-import cacheUtil from '../src/server/ApiRoutes/cacheUtil';
+import cacheMethods from '../src/server/ApiRoutes/cacheUtil';
 
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 
 chai.use(chaiAsPromised);
+
 const {
   expect,
 } = chai;
 
 describe('cacheUtil', () => {
-  let values;
+  let calledWith;
   let mockClient;
+  let values;
+  let mockDataFunction;
   let checkForKeyInRedis;
   let getDataAndSetKeyInClient;
   let useCachedOrGetData;
+  let addCaching;
   let getSearchData;
-  let cache;
-  let mockDataFunction;
+  let getKeyFromParams;
 
   beforeEach(() => {
+    calledWith = [];
     mockDataFunction = (url) => {
+      calledWith.push(url);
       let resolveTo;
       switch (url) {
         case 'elephant':
@@ -48,32 +52,46 @@ describe('cacheUtil', () => {
       },
     };
 
-    cache = cacheUtil(mockDataFunction, false, mockClient);
-    checkForKeyInRedis = cache.checkForKeyInRedis;
-    getDataAndSetKeyInClient = cache.getDataAndSetKeyInClient;
-    useCachedOrGetData = cache.useCachedOrGetData;
-    getSearchData = cache.getSearchData;
+    checkForKeyInRedis = cacheMethods.checkForKeyInRedis(mockClient);
+    getDataAndSetKeyInClient = cacheMethods.getDataAndSetClientKey(mockDataFunction, mockClient);
+    useCachedOrGetData = cacheMethods.useCachedOrGetData(mockDataFunction, mockClient);
+    addCaching = cacheMethods.addCaching;
+    getSearchData = addCaching(mockDataFunction, mockClient);
+    getKeyFromParams = cacheMethods.getKeyFromParams;
   });
 
   describe('checkForKeyInRedis', () => {
     it('should resolve to a value that has been set', () => {
       mockClient.set('hello', 'world');
-      return expect(checkForKeyInRedis('hello')).to.eventually.equal('world');
+      return expect(checkForKeyInRedis('hello'))
+        .to
+        .eventually
+        .equal('world');
     });
 
     it('should resolve to null for a value that has not been set', () => {
-      return expect(checkForKeyInRedis('goodbye')).to.eventually.equal(null);
+      return expect(checkForKeyInRedis('goodbye'))
+        .to
+        .eventually
+        .equal(null);
     });
   });
 
   describe('getDataAndSetKeyInClient', () => {
     it('should return a stringified version of the response', () => {
-      return expect(getDataAndSetKeyInClient('elephant')).to.eventually.equal('{"loxodonta":"africana"}');
+      const params = ['elephant'];
+      const key = getKeyFromParams(params);
+      return expect(getDataAndSetKeyInClient(params, key))
+        .to
+        .eventually
+        .equal('{"loxodonta":"africana"}');
     });
 
     it('should assign the stringified response to the key of the url', () => {
-      return expect(getDataAndSetKeyInClient('elephant')
-        .then(() => values.elephant))
+      const params = ['elephant'];
+      const key = getKeyFromParams(params);
+      return expect(getDataAndSetKeyInClient(params, key)
+        .then(() => values[key]))
         .to
         .eventually
         .equal('{"loxodonta":"africana"}');
@@ -82,34 +100,32 @@ describe('cacheUtil', () => {
 
   describe('useCachedOrGetData', () => {
     it('should call the data function for a new key', () => {
-      const spy = sinon.spy(cache, 'getDataAndSetKeyInClient');
-      return expect(useCachedOrGetData('elephant')
-        .then(() => spy.called))
+      return expect(useCachedOrGetData(['elephant'])
+        .then(() => calledWith.length === 1))
         .to
         .eventually
         .equal(true);
     });
 
     it('should return the correct value for a new key', () => {
-      return expect(useCachedOrGetData('elephant'))
+      return expect(useCachedOrGetData(['elephant']))
         .to
         .eventually
         .equal('{"loxodonta":"africana"}');
     })
 
     it('should not call the data function for an old key', () => {
-      const spy = sinon.spy(cache, 'getDataAndSetKeyInClient');
-      return expect(useCachedOrGetData('elephant')
-        .then(() => useCachedOrGetData('elephant'))
-        .then(() => spy.calledOnce))
+      return expect(useCachedOrGetData(['elephant'])
+        .then(() => useCachedOrGetData(['elephant']))
+        .then(() => calledWith.length === 1))
         .to
         .eventually
         .equal(true);
     });
 
     it('should return the correct value for an old key', () => {
-      return expect(useCachedOrGetData('elephant')
-        .then(useCachedOrGetData('elephant')))
+      return expect(useCachedOrGetData(['elephant'])
+        .then(useCachedOrGetData(['elephant'])))
         .to
         .eventually
         .equal('{"loxodonta":"africana"}');
