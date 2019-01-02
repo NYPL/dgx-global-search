df1f432de70e94b729f2ad6b65af24b0264f3188
Daniel Appel <dan.s.appel@gmail.com>
2019-01-02 14:21:31 -0500

    Rename client property of clientWrapper to rawClient

--- src/server/ApiRoutes/clientWrapper.js
+++ src/server/ApiRoutes/clientWrapper.js
@@ -1,24 +1,24 @@
 import redis from 'redis';
 
 function ClientWrapper() {
-  this.client = redis.createClient();
+  this.rawClient = redis.createClient();
   this.connected = false;
-  this.client.on('connect', () => {
+  this.rawClient.on('connect', () => {
     this.connected = true;
     console.log('REDIS CONNECT');
   });
-  this.client.on('error', (err) => {
+  this.rawClient.on('error', (err) => {
     this.connected = false;
     console.log('REDIS ERROR', err);
   });
-  this.client.on('reconnecting', (delay) => {
+  this.rawClient.on('reconnecting', (delay) => {
     console.log('REDIS RECONNECTING: ', delay);
   });
   this.get = (key, callback) => (this.connected
-    ? this.client.get(key, callback)
+    ? this.rawClient.get(key, callback)
     : callback(null, null));
   this.set = (key, value, code, timeout) => (this.connected
-    ? this.client.set(key, value, code, timeout)
+    ? this.rawClient.set(key, value, code, timeout)
     : null);
 }
 
