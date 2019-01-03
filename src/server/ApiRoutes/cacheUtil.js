05c7bcc9f117a9f204bbe6287974812b7f3385d7
Daniel Appel <dan.s.appel@gmail.com>
2019-01-03 11:45:38 -0500

    Add descriptive comments to methods in cacheUtil

--- src/server/ApiRoutes/cacheUtil.js
+++ src/server/ApiRoutes/cacheUtil.js
@@ -6,10 +6,39 @@ const {
   redisHosts,
 } = appConfig;
 
+/**
+ Takes a list of params and returns a string which will be used as a key
+ in the caching client.
+ @param {array} params
+ */
+
 const getKeyFromParams = params => params.map(x => JSON.stringify(x)).join('');
+
+/**
+ checkForKeyInRedis(client)
+ Takes a caching client (e.g. redis). Returns a function which takes a key and
+ returns a promise that either resolves to the stored value for that key, or null
+ if there is no stored value.
+
+ @param {object} client
+ @param {string} key
+ */
 
 const checkForKeyInRedis = client => key => new Promise(resolve => client
   .get(key, (err, cachedResponse) => resolve(cachedResponse)));
+
+/**
+ getDataAndSetKeyInClient(dataFunction, client)
+ Takes a dataFunction, which fetches new data (e.g. a function which calls axios)
+ and a caching client (e.g. redis). Returns a function which takes some params
+ and a key, fetches the corresponding data for those params, and caches that
+ data under the given key
+
+ @param {function} dataFunction
+ @param {object} client
+ @param {array} params
+ @param {string} key
+ */
 
 const getDataAndSetKeyInClient = (dataFunction, client) => (params, key) => dataFunction(...params)
   .then((response) => {
@@ -18,6 +47,18 @@ const getDataAndSetKeyInClient = (dataFunction, client) => (params, key) => data
     client.set(key, stringifiedResponse, 'EX', 3600);
     return stringifiedResponse;
   });
+
+/**
+ useCachedOrGetData(dataFunction, client)
+ Takes a dataFunction, which fetches new data (e.g. a function which calls axios)
+ and a caching client (e.g. redis). Returns a function which takes params, checks
+ to see if those params are cached, and then either returns the cached value or gets
+ a new value for those params if none is cached
+
+ @param {function} dataFunction
+ @param {object} client
+ @param {array} params
+ */
 
 const useCachedOrGetData = (dataFunction, client) => (params) => {
   const key = getKeyFromParams(params);
@@ -29,6 +70,18 @@ const useCachedOrGetData = (dataFunction, client) => (params) => {
     );
 };
 
+/**
+ addCaching(dataFunction, useClient, customClient)
+ Returns a promise which will resolve to a function that wraps the given
+ datafunction. The wrapper will call useCachedOrGetData with the given datafunction
+ and to ensure the value is cached, and then return the value.
+ addCaching will also generate the dataclient if a custom client is not given and
+ useClient is not set to false.
+
+ @param {function} datafunction
+ @param {boolean} useClient
+ @param {object} customClient
+ */
 
 const addCaching = (dataFunction, useClient = true, customClient = null) => {
   if (!useClient) {
