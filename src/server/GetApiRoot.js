238394662311aae5e574fbf8b8a6344fdcd37f92
Kang Peng <kangtingpeng@nypl.org>
2019-01-02 19:32:51 -0500

    Linted files.

--- src/server/GetApiRoot.js
+++ src/server/GetApiRoot.js
@@ -1,5 +1,5 @@
 import appConfig from '../../appConfig';
-import aws from './../app/utils/kms-helper.js';
+import aws from '../app/utils/kms-helper';
 
 /**
  * getApiRoot(req, res, next)
@@ -10,44 +10,41 @@ import aws from './../app/utils/kms-helper.js';
  * @param {object} next
  * @return {function} - The returned function takes parameters as below,
  * app - The Express instance
- * api_root_env {string} - The environment variable of API_ROOT
- * app_env {string} - The environment variable of APP_ENV
- * region_env {string} - The environment variable of REGION_ENV
+ * apiRootEnv {string} - The environment variable of API_ROOT
+ * appEnv {string} - The environment variable of APP_ENV
+ * regionEnv {string} - The environment variable of REGION_ENV
  */
-const getApiRoot = (req, res, next) => {
-  return (app, api_root_env, app_env, region_env) => {
-    // Skips calling AWS if we already have API root
-    if (app.locals.apiRoot) {
+const getApiRoot = (req, res, next) => (app, apiRootEnv, appEnv, regionEnv) => {
+  // Skips calling AWS if we already have API root
+  if (app.locals.apiRoot) {
+    next();
+  } else {
+    // Assigns API_ROOT env variable to the API root if the app receives it
+    if (apiRootEnv) {
+      app.locals.apiRoot = apiRootEnv;
       next();
-    } else {
-      // Assigns API_ROOT env variable to the API root if the app receives it
-      if (api_root_env) {
-        app.locals.apiRoot = api_root_env;
-        next();
-      // If we do not have API_ROOT env variable or existing API root, call AWS to decrypt the one
-      // in appConfig.js
-      // We have to have valid AWS crendentials locally to do that
-      } else {
-          const encryptApiUrl = app_env === 'development'
-            ? appConfig.developmentUrl : appConfig.productionUrl;
-          const awsProfile = app_env === 'development'
-            ? 'nypl-sandbox' : 'nypl-digital-dev';
-          const region = region_env || 'us-east-1';
-
-          // set API_ROOT to the correct encrypted value
-          aws.decrypt(encryptApiUrl, awsProfile, region)
-            .then((decryptApiRoot) => {
-              app.locals.apiRoot = decryptApiRoot.slice(1, decryptApiRoot.length - 1);
-              next();
-            })
-            .catch(error => {
-              console.log(`error getting API root: ${error}`);
-              app.locals.apiRoot = undefined;
-              next();
-            });
-      }
     }
-  };
+    // If we do not have API_ROOT env variable or existing API root, call AWS to decrypt the one
+    // in appConfig.js
+    // We have to have valid AWS crendentials locally to do that
+    const encryptApiUrl = appEnv === 'development'
+      ? appConfig.developmentUrl : appConfig.productionUrl;
+    const awsProfile = appEnv === 'development'
+      ? 'nypl-sandbox' : 'nypl-digital-dev';
+    const region = regionEnv || 'us-east-1';
+
+    // set API_ROOT to the correct encrypted value
+    aws.decrypt(encryptApiUrl, awsProfile, region)
+      .then((decryptApiRoot) => {
+        app.locals.apiRoot = decryptApiRoot.slice(1, decryptApiRoot.length - 1);
+        next();
+      })
+      .catch((error) => {
+        console.log(`error getting API root: ${error}`);
+        app.locals.apiRoot = undefined;
+        next();
+      });
+  }
 };
 
 export default getApiRoot;
