76ea635663da72bec9ea8c73d7d6950eab1515d4
Daniel Appel <dan.s.appel@gmail.com>
2019-01-04 14:50:55 -0500

    Change require to import for chai in cacheUtil tests

--- test/cacheUtil.test.js
+++ test/cacheUtil.test.js
@@ -1,6 +1,6 @@
+import chai from 'chai';
 import cacheMethods from '../src/server/ApiRoutes/cacheUtil';
 
-const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 
 chai.use(chaiAsPromised);
