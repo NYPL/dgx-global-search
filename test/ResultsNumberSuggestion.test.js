a4c8a77b427a40cab852305f258f5a948ae63bd4
Daniel Appel <dan.s.appel@gmail.com>
2019-01-15 17:10:39 -0500

    Add more tests for ResultsNumberSuggestion

--- test/ResultsNumberSuggestion.test.js
+++ test/ResultsNumberSuggestion.test.js
@@ -4,6 +4,12 @@ import { expect } from 'chai';
 import ResultsNumberSuggestion from '../src/app/components/ResultsNumberSuggestion/ResultsNumberSuggestion';
 
 describe('ResultsNumberSuggestion', () => {
+  const tabs = [
+    { anchor: 'All', label: 'all_results', resultSummarydisplayName: '' },
+    { anchor: 'Database', label: 'articles_databases', resultSummarydisplayName: 'databases' },
+    { anchor: 'Events', label: 'events_classes', resultSummarydisplayName: 'events' },
+  ];
+
   describe('if the user visits the main page without a keyword', () => {
     let component;
 
@@ -57,6 +63,7 @@ describe('ResultsNumberSuggestion', () => {
           searchKeyword="jibberish"
           isKeywordValid
           amount="0"
+          resultsLength="0"
         />,
       );
     });
@@ -72,47 +79,92 @@ describe('ResultsNumberSuggestion', () => {
     });
   });
 
+  describe('if there is a search keyword and a facet other than "All" is selected, '
+      + 'yet there are no matching search results', () => {
+    let component;
+
+    before(() => {
+      component = shallow(
+        <ResultsNumberSuggestion
+          searchKeyword="jibberish"
+          selectedFacet="events_classes"
+          isKeywordValid
+          amount="0"
+          resultsLength="0"
+          tabs={tabs}
+        />,
+      );
+    });
+
+    it('should render "No results were found in selected_facet_name".', () => {
+      expect(component.find('#search-results-summary').text()).to.deep.equal(
+        'No results were found in events',
+      );
+      expect(component.find('#search-results-summary').hasClass('noResultMessage'))
+        .to.equal(true);
+      expect(component.find('#search-results-summary').hasClass('results-length'))
+        .to.equal(false);
+    });
+  });
+
+  describe('if there is a search keyword and the facet "All" is selected'
+    + 'yet there are no search results', () => {
+    let component;
+
+    before(() => {
+      component = shallow(
+        <ResultsNumberSuggestion
+          tabs={tabs}
+          searchKeyword="jibberish"
+          selectedFacet=""
+          resultsLength="0"
+          amount="0"
+          isKeywordValid
+        />,
+      );
+    });
+
+    it('should render "No results were found".', () => {
+      expect(component.find('#search-results-summary').text()).to.deep.equal(
+        'No results were found',
+      );
+      expect(component.find('#search-results-summary').hasClass('noResultMessage'))
+        .to.equal(true);
+      expect(component.find('#search-results-summary').hasClass('results-length'))
+        .to.equal(false);
+    });
+  });
+
+  describe('if more than 1 results are returned and a facet other than "All is selected"', () => {
+    let component;
+    const results = [{},{},{}];
+    const amount = results.length.toString();
+
+    before(() => {
+      component = shallow(
+        <ResultsNumberSuggestion
+          tabs={tabs}
+          amount={amount}
+          resultsLength={amount}
+          searchKeyword="okapi"
+          selectedFacet="articles_databases"
+          isKeywordValid
+        />,
+      );
+    });
+
+    it('should render the proper summary for the results.', () => {
+      expect(component.find('#search-results-summary').text()).to.deep.equal(
+        'Found about 3 results for "okapi" in databases',
+      );
+      expect(component.find('#search-results-summary').hasClass('noResultMessage'))
+        .to.equal(false);
+      expect(component.find('#search-results-summary').hasClass('results-length'))
+        .to.equal(true);
+    });
+  });
 
 });
-      //
-      //
-      //       it('should render "No results were found in selected_facet_name".', () => {
-      //         expect(component.find('#search-results-summary').text()).to.deep.equal(
-      //           'No results were found in events',
-      //         );
-      //         expect(component.find('#search-results-summary').hasClass('noResultMessage'))
-      //           .to.equal(true);
-      //         expect(component.find('#search-results-summary').hasClass('results-length'))
-      //           .to.equal(false);
-      //       });
-      //
-      //
-      //             it('should render "No results were found".', () => {
-      //               expect(component.find('#search-results-summary').text()).to.deep.equal(
-      //                 'No results were found',
-      //               );
-      //               expect(component.find('#search-results-summary').hasClass('noResultMessage'))
-      //                 .to.equal(true);
-      //               expect(component.find('#search-results-summary').hasClass('results-length'))
-      //                 .to.equal(false);
-      //             });
-      //
-      //
-      //                   it('should render the proper summary for the results.', () => {
-      //                     expect(component.find('#search-results-summary').text()).to.deep.equal(
-      //                       'Found about 3 results for "okapi" in databases',
-      //                     );
-      //                     expect(component.find('#search-results-summary').hasClass('noResultMessage'))
-      //                       .to.equal(false);
-      //                     expect(component.find('#search-results-summary').hasClass('results-length'))
-      //                       .to.equal(true);
-      //                   });
-      //
-      //                   it('should display a result page with valid results.', () => {
-      //                     expect(component.find('#results').children()).to.have.length(3);
-      //                   });
-      //
-      //
       //                         it('should render the proper summary for the results.', () => {
       //                           expect(component.find('#search-results-summary').text()).to.deep.equal(
       //                             'Found about 3 results for "okapi"',
