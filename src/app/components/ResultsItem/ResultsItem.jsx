754624826b360dd668d570479df0306bcda9c659
Kang Peng <kangtingpeng@nypl.org>
2019-01-16 18:52:15 -0500

    Linted ResultsItem.jsx

--- src/app/components/ResultsItem/ResultsItem.jsx
+++ src/app/components/ResultsItem/ResultsItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { generateSearchedFrom, nativeGA } from '../../utils/GAUtils.js';
+import { generateSearchedFrom, nativeGA } from '../../utils/GAUtils';
 
 class ResultsItem extends React.Component {
   constructor(props) {
@@ -53,10 +53,14 @@ class ResultsItem extends React.Component {
    * @param {object} event
    */
   triggerGASend(index, target, event) {
+    const {
+      link,
+    } = this.props;
+
     if (event) {
       if (event.keyCode === 13 || event.key === 'Enter') {
         this.sendGAClickthroughEvent(index, target, event);
-        window.location = this.props.link;
+        window.location = link;
       }
     }
   }
@@ -71,9 +75,17 @@ class ResultsItem extends React.Component {
    * @param {object} event - The event happened to the element
    */
   sendGAClickthroughEvent(index, target, event) {
+    const {
+      isGAClickThroughClicked,
+      updateGAClickThroughClicked,
+      searchKeyword,
+      timeToLoadResults,
+      queriesForGA,
+    } = this.props;
+
     // Check if a click through has already happened once. We only send the first click through
-    if (!this.props.isGAClickThroughClicked) {
-      this.props.updateGAClickThroughClicked(true);
+    if (!isGAClickThroughClicked) {
+      updateGAClickThroughClicked(true);
       // Index is 0-based, we need ordinality to start at 1.
       const ordinality = (Number.isInteger(index)) ? index + 1 : 0;
       let clickTarget = target;
@@ -81,7 +93,7 @@ class ResultsItem extends React.Component {
 
       // Only on the first 10 results we want to track the CTR event
       if (ordinality < 11) {
-              // Detect the key click combo and add the result to Dimension3/ClickTarget
+        // Detect the key click combo and add the result to Dimension3/ClickTarget
         if (event) {
           if (event.ctrlKey || event.metaKey) {
             clickTarget += 'Keyed';
@@ -98,16 +110,16 @@ class ResultsItem extends React.Component {
           isCTRSent = true;
           nativeGA(
             'Clickthrough',
-            this.props.searchKeyword,
+            searchKeyword,
             ordinality,
-            generateSearchedFrom(this.props.timeToLoadResults, this.props.queriesForGA),
+            generateSearchedFrom(timeToLoadResults, queriesForGA),
             clickTarget,
             () => {
               setTimeout(() => { isCTRSent = false; }, 200);
               if (event.isDefaultPrevented) {
                 event.target.click();
               }
-            }
+            },
           );
         }
       }
@@ -126,6 +138,9 @@ class ResultsItem extends React.Component {
    * @return {object}
    */
   renderTitle(title, className, wholeRowClass) {
+    const {
+      index,
+    } = this.props;
     const newTitle = title || 'No Title for this Item';
     const visuallyHiddenClass = title ? '' : 'visuallyHidden';
 
@@ -134,31 +149,33 @@ class ResultsItem extends React.Component {
         className={`${className}-title ${wholeRowClass} ${visuallyHiddenClass}`}
         dangerouslySetInnerHTML={this.createMarkup(newTitle)}
         onClick={(e) => {
-          this.sendGAClickthroughEvent(this.props.index, 'ResultTitle', e);
+          this.sendGAClickthroughEvent(index, 'ResultTitle', e);
         }}
         // Add the event listener to right click
         onContextMenu={(e) => {
           this.sendGAClickthroughEvent(
-            this.props.index,
+            index,
             'ResultTitleContextmenu',
-            e
+            e,
           );
         }}
-      >
-      </h2>
+      />
     );
   }
 
   /**
-   * renderImage(className, src, title)
+   * renderImage(className, src)
    * The function renders <img> if this.props.thumbnailSrc is true.
    *
    * @param {string} className
    * @param {string} src
-   * @param {string} title
    * @return null or {object}
    */
-  renderImage(className, src, title) {
+  renderImage(className, src) {
+    const {
+      index,
+    } = this.props;
+
     if (!src) {
       return null;
     }
@@ -167,15 +184,15 @@ class ResultsItem extends React.Component {
       <div
         className={`${className}-imageWrapper`}
         onClick={(e) => {
-          this.sendGAClickthroughEvent(this.props.index, 'ResultPicture', e);
+          this.sendGAClickthroughEvent(index, 'ResultPicture', e);
         }}
 
         // Add the event listener to right click
         onContextMenu={(e) => {
           this.sendGAClickthroughEvent(
-            this.props.index,
+            index,
             'ResultPictureContextmenu',
-            e
+            e,
           );
         }}
       >
@@ -189,37 +206,47 @@ class ResultsItem extends React.Component {
   }
 
   render() {
-    const wholeRowClass = this.generateWholeRowClass(this.props.thumbnailSrc);
+    const {
+      id,
+      label,
+      index,
+      thumbnailSrc,
+      title,
+      link,
+      className,
+      snippet,
+    } = this.props;
+
+    const wholeRowClass = this.generateWholeRowClass(thumbnailSrc);
 
     return (
       <li
-        id={`${this.props.id}-${this.props.index}`}
-        className={`${this.props.className} ${wholeRowClass}`}
+        id={`${id}-${index}`}
+        className={`${className} ${wholeRowClass}`}
       >
         <p
-          className={`${this.props.className}-label ${wholeRowClass}`}
+          className={`${className}-label ${wholeRowClass}`}
         >
-          {this.props.label}
+          {label}
         </p>
         <a
-          className={`${this.props.className}-link ${wholeRowClass}`}
-          href={this.props.link}
-          ref={`result-${this.props.index}-item`}
-          onKeyDown={(e) => { this.triggerGASend(this.props.index, 'ResultTitle', e); }}
+          className={`${className}-link ${wholeRowClass}`}
+          href={link}
+          ref={`result-${index}-item`}
+          onKeyDown={(e) => { this.triggerGASend(index, 'ResultTitle', e); }}
         >
-          {this.renderImage(this.props.className, this.props.thumbnailSrc, this.props.title)}
-          {this.renderTitle(this.props.title, this.props.className, wholeRowClass)}
+          {this.renderImage(className, thumbnailSrc, title)}
+          {this.renderTitle(title, className, wholeRowClass)}
         </a>
         <p
-          className={`${this.props.className}-linkText ${wholeRowClass}`}
+          className={`${className}-linkText ${wholeRowClass}`}
         >
-          {this.props.link}
+          {link}
         </p>
         <p
-          className={`${this.props.className}-snippet ${wholeRowClass}`}
-          dangerouslySetInnerHTML={this.createMarkup(this.props.snippet)}
-        >
-        </p>
+          className={`${className}-snippet ${wholeRowClass}`}
+          dangerouslySetInnerHTML={this.createMarkup(snippet)}
+        />
       </li>
     );
   }
@@ -234,19 +261,25 @@ ResultsItem.propTypes = {
   snippet: PropTypes.string,
   thumbnailSrc: PropTypes.string,
   label: PropTypes.string,
-  wholeRowClass: PropTypes.string,
   isGAClickThroughClicked: PropTypes.bool,
   updateGAClickThroughClicked: PropTypes.func,
   searchKeyword: PropTypes.string,
   timeToLoadResults: PropTypes.number,
-  queriesForGA: PropTypes.object,
+  queriesForGA: PropTypes.objectOf(PropTypes.any),
 };
 
 ResultsItem.defaultProps = {
-  lang: 'en',
   id: 'resultsItem',
   className: 'resultsItem',
   index: 0,
+  title: '',
+  link: '',
+  snippet: '',
+  thumbnailSrc: '',
+  label: '',
+  isGAClickThroughClicked: false,
+  updateGAClickThroughClicked: () => {},
+  searchKeyword: '',
   timeToLoadResults: '',
   queriesForGA: {},
 };
