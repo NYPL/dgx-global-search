ddd2b72e58b8cee5a056e053942d3bab820a09c1
Kang Peng <kangtingpeng@nypl.org>
2019-01-23 14:29:21 -0500

    Linted test/SearchModelTestExampleData.js

--- test/SearchModelTestExampleData.js
+++ test/SearchModelTestExampleData.js
@@ -1,4 +1,4 @@
-import filterNames from '../src/app/utils/FilterNames.js';
+import filterNames from '../src/app/utils/FilterNames';
 
 const testData = {
   noItem: {
@@ -20,16 +20,17 @@ const testData = {
         link: 'https://gethelp.nypl.org/customer/portal/articles/2214923',
         pagemap: {
           cse_thumbnail: [
-            { src: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:' +
-              'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM'
-            }
-          ]
+            {
+              src: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:'
+              + 'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
+            },
+          ],
         },
-        snippet: 'Jun 23, 2016 ... &lt;i&gt;SimplyE requires an Apple ID&lt;/i&gt; and a device ' +
-          'with iOS8 or higher in order to download &lt;br&gt;the SimplyE app. iPad, iPad 2, '+
-          'iPad Air, iPad mini iOS 8+ ...&lt;/div',
-        'thumbnail-url': 'https://encrypted-tbn3.gstatic.com/images?q=tbn:' +
-          'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
+        snippet: 'Jun 23, 2016 ... &lt;i&gt;SimplyE requires an Apple ID&lt;/i&gt; and a device '
+          + 'with iOS8 or higher in order to download &lt;br&gt;the SimplyE app. iPad, iPad 2, '
+          + 'iPad Air, iPad mini iOS 8+ ...&lt;/div',
+        'thumbnail-url': 'https://encrypted-tbn3.gstatic.com/images?q=tbn:'
+          + 'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
         labels: [
           {
             name: 'exhibitions',
@@ -54,16 +55,17 @@ const testData = {
         link: 'http://gethelp.nypl.org/customer/portal/articles/2214923',
         pagemap: {
           cse_thumbnail: [
-            { src: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:' +
-              'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM'
-            }
-          ]
+            {
+              src: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:'
+              + 'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
+            },
+          ],
         },
-        snippet: 'Jun 23, 2016 ... &lt;i&gt;SimplyE requires an Apple ID&lt;/i&gt; and a device ' +
-          'with iOS8 or higher in order to download &lt;br&gt;the SimplyE app. iPad, iPad 2, '+
-          'iPad Air, iPad mini iOS 8+ ...&lt;/div',
-        'thumbnail-url': 'https://encrypted-tbn3.gstatic.com/images?q=tbn:' +
-          'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
+        snippet: 'Jun 23, 2016 ... &lt;i&gt;SimplyE requires an Apple ID&lt;/i&gt; and a device '
+          + 'with iOS8 or higher in order to download &lt;br&gt;the SimplyE app. iPad, iPad 2, '
+          + 'iPad Air, iPad mini iOS 8+ ...&lt;/div',
+        'thumbnail-url': 'https://encrypted-tbn3.gstatic.com/images?q=tbn:'
+          + 'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
         labels: [
           {
             name: 'exhibitions',
@@ -88,16 +90,17 @@ const testData = {
         link: 'http://exhibitions.nypl.org/biblion/worldsfair/gallery/gallery-aquacade',
         pagemap: {
           cse_thumbnail: [
-            { src: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:' +
-              'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM'
-            }
-          ]
+            {
+              src: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:'
+              + 'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
+            },
+          ],
         },
-        snippet: 'Jun 23, 2016 ... &lt;i&gt;SimplyE requires an Apple ID&lt;/i&gt; and a device ' +
-          'with iOS8 or higher in order to download &lt;br&gt;the SimplyE app. iPad, iPad 2, '+
-          'iPad Air, iPad mini iOS 8+ ...&lt;/div',
-        'thumbnail-url': 'https://encrypted-tbn3.gstatic.com/images?q=tbn:' +
-          'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
+        snippet: 'Jun 23, 2016 ... &lt;i&gt;SimplyE requires an Apple ID&lt;/i&gt; and a device '
+          + 'with iOS8 or higher in order to download &lt;br&gt;the SimplyE app. iPad, iPad 2, '
+          + 'iPad Air, iPad mini iOS 8+ ...&lt;/div',
+        'thumbnail-url': 'https://encrypted-tbn3.gstatic.com/images?q=tbn:'
+          + 'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
         labels: [
           {
             name: 'exhibitions',
@@ -131,11 +134,11 @@ const matchedResults = {
     {
       title: 'Get Started with SimplyE for Apple iOS',
       link: 'https://gethelp.nypl.org/customer/portal/articles/2214923',
-      snippet: 'Jun 23, 2016 ... SimplyE requires an Apple ID and a device with iOS8 or ' +
-        'higher in order to download the SimplyE app. iPad, iPad 2, iPad Air, iPad mini ' +
-        'iOS 8+ ...',
-      thumbnailSrc: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:' +
-        'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
+      snippet: 'Jun 23, 2016 ... SimplyE requires an Apple ID and a device with iOS8 or '
+        + 'higher in order to download the SimplyE app. iPad, iPad 2, iPad Air, iPad mini '
+        + 'iOS 8+ ...',
+      thumbnailSrc: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:'
+        + 'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
       label: 'Exhibitions',
     },
   ],
@@ -143,11 +146,11 @@ const matchedResults = {
     {
       title: 'Get Started with SimplyE for Apple iOS',
       link: 'http://exhibitions.nypl.org/biblion/worldsfair/gallery/gallery-aquacade',
-      snippet: 'Jun 23, 2016 ... SimplyE requires an Apple ID and a device with iOS8 or ' +
-        'higher in order to download the SimplyE app. iPad, iPad 2, iPad Air, iPad mini ' +
-        'iOS 8+ ...',
-      thumbnailSrc: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:' +
-        'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
+      snippet: 'Jun 23, 2016 ... SimplyE requires an Apple ID and a device with iOS8 or '
+        + 'higher in order to download the SimplyE app. iPad, iPad 2, iPad Air, iPad mini '
+        + 'iOS 8+ ...',
+      thumbnailSrc: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:'
+        + 'ANd9GcScCym1VJKFD1kWJG7Pl4hwD0PaKv2iLi8FfV4g2z4J7fg0EN_MnqPaXqJM',
       label: 'Exhibitions',
     },
   ],
